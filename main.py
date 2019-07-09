37ddc20ed8fc6b88e4bad35f9c764e62dd6f523c
Matej Sokol <sokolvm@gmail.com>
2019-07-09 19:51:00 +0200

    fixing imports

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from bubbly import bubble
+from divide_for_while import for_loop_divide, while_loop
+from in_the_matrix import sum_all_seven, sum_specific_seven
+from palim_dron import palindrome
+
 if __name__ == '__main__':
     sum_all_seven()
     sum_specific_seven()
