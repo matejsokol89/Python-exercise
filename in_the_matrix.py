c3b5c3047d50de9cee7abeaf017afa600bdc98aa
Matej Sokol <sokolvm@gmail.com>
2019-07-09 19:52:28 +0200

    finding number seven and sum up sevens

---
+++ in_the_matrix.py
@@ -0,0 +1,28 @@
+from typing import List
+
+a: List[List[int]] = [
+    [7, 4, 3],
+    [1, 1, 7],
+    [1, 7, 7]
+]
+
+
+def sum_all_seven():
+    sum_up: int = 0
+    for i in range(0, len(a)):
+        for index in a[i]:
+            if index == 7:
+                sum_up: int = sum_up + index
+    print(sum_up)
+
+
+def sum_specific_seven():
+    flat_list: List[int] = []
+    sum_up_seven: int = 0
+    for i in range(0, len(a)):
+        for res in a[i]:
+            flat_list.append(res)
+    for i in range(0, len(flat_list)):
+        if flat_list[i] == 1 and flat_list[i + 1] == 7:
+            sum_up_seven: int = sum_up_seven + flat_list[i + 1]
+    print(sum_up_seven)
