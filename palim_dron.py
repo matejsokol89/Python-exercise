e63cfaaf337b8cb4359953cd8b831eacf3e3f429
Matej Sokol <sokolvm@gmail.com>
2019-07-09 19:51:47 +0200

    palindrome function

---
+++ palim_dron.py
@@ -0,0 +1,23 @@
+def palindrome():
+    global first_order, back_order, divide
+    first_order = ""
+    back_order = ""
+    word = "perorep"
+    lenght: int = int(len(word) / 2 + 1)
+    reverse: str = ""
+
+    for i in range(0, lenght):
+        first_order = first_order + word[i]
+    print(first_order)
+
+    for i in range(len(word), 0, -1):
+        reverse = reverse + word[i - 1]
+        divide = int(len(reverse) / 2 + 1)
+
+    for j in range(0, divide):
+        back_order = back_order + reverse[j]
+    print(back_order)
+    if first_order == back_order:
+        print(True)
+    else:
+        print(False)
